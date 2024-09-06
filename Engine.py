fceabacf90ed09164ac1927f2cc3f1208c44035c
David Schmidle <137837519+Dbschmidle@users.noreply.github.com>
2024-09-06 18:13:32 -0400

    Refactoring board.py to Engine.py

---
+++ Engine.py
@@ -0,0 +1,22 @@
+"""
+Stores the information about the current state of a chess game.
+Determines the valid moves for the current gamestate.
+
+TODO: Use numpy for faster processing, especially when implementing AI engine
+"""
+class GameState():
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
+        
+        self.whiteToMove = True
+        self.moveLog = []
+        
