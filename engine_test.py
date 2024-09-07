90fde62ff6ce0e0154cb5117e893212714657f37
David Schmidle <137837519+Dbschmidle@users.noreply.github.com>
2024-09-07 15:09:58 -0400

    Added Move conversions from rows, cols to rank, files

--- engine_test.py
+++ engine_test.py
@@ -1,15 +1,90 @@
+"""
+Unit testing for the Engine.py file.
+
+Tests should be written BEFORE writting the method code
+"""
 
 import src
 
 def main():
-    toChessNotation()
+    a, b, c = test_convertToRankFile()
+    printPassed(a, b, c)
+    a, b, c = test_convertToChessNotation()
+    printPassed(a, b, c)
 
 
-def toChessNotation():
-    m = src.Move((6, 4), (4, 4), None)
-    print(m.convertToChessNotation())
-        
+def printPassed(fname: str, numPassed: int, numTests: int):
+    print(fname +": "+ str(numPassed)+"/"+str(numTests)+" tests passed.")
+
+
+def test_convertToRankFile() -> tuple[str, int, int]:
+    fname = "convertToRankFile"
+    gamestate = src.GameState()
+    m = src.Move((6, 4), (4, 4), gamestate.board)
     
+    test_inputs = {
+        0: [6, 4],
+        1: [4, 4],
+        2: [0, 0]
+    }
+    
+    expected_outputs = {
+        0: "e2",
+        1: "e4",
+        2: "a8"
+    }
+    
+    passed = 0
+    numTests = len(expected_outputs)
+    
+    
+    for testNum in test_inputs:
+        actual_output = m.convertToRankFile(test_inputs[testNum][0], test_inputs[testNum][1])
+        
+        if (actual_output == expected_outputs[testNum]):
+            passed += 1
+            
+        else:
+            print("Test #"+str(testNum)+" failed. \n\tExpected: "+str(expected_outputs[testNum])+" \n\tActual: "+actual_output)
+            
+    return fname, passed, numTests
+    
+
+def test_convertToChessNotation() -> tuple[str, int, int]:
+    fname = "convertToChessNotation"
+    gamestate = src.GameState()
+    m = src.Move((6, 4), (4, 4), gamestate.board)
+    
+    test_inputs = {
+        0: [[6, 4], [4, 4]],
+        1: [[1, 0], [3, 0]]
+    }
+    
+    expected_outputs = {
+        0: "e2e4",
+        1: "a7a5",
+    }
+    
+    passed = 0
+    numTests = len(expected_outputs)
+    
+    
+    for testNum in test_inputs:
+        m.fromRow = test_inputs[testNum][0][0]
+        m.fromCol = test_inputs[testNum][0][1]
+        m.toRow = test_inputs[testNum][1][0]
+        m.toCol = test_inputs[testNum][1][1]
+        
+        actual_output = m.convertToChessNotation()
+        
+        if (actual_output == expected_outputs[testNum]):
+            passed += 1
+            
+        else:
+            print("\""+fname+"\""+ ": Test "+str(testNum)+" failed. \n\tExpected: "+str(expected_outputs[testNum])+" \n\tActual: "+actual_output)
+            
+    return fname, passed, numTests
+
     
 if __name__ == "__main__":
     main()
--- src/Engine.py
+++ src/Engine.py
@@ -1,9 +1,46 @@
+
+         
+"""
+Stores the information of a move
+"""
+class Move():
+    CONV_RANK_TO_ROWS = {"1":7, "2":6, "3":5, "4":4, "5":3, "6":2, "7":1, "8":0}
+    CONV_FILES_TO_COLS = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7}
+    CONV_ROWS_TO_RANK = {7: "1", 6:"2", 5:"3", 4:"4", 3:"5", 2:"6", 1:"7", 0:"8"}
+    CONV_COLS_TO_FILES = {7: "h", 6:"g", 5:"f", 4:"e", 3:"d", 2:"c", 1:"b", 0:"a"}
+    
+    
+    def __init__(self, fromSquare: tuple[int, int], toSquare: tuple[int, int], board: list[list[str]]):
+        self.fromRow = fromSquare[0]
+        self.fromCol = fromSquare[1]
+        self.toRow = toSquare[0]
+        self.toCol = toSquare[1]
+        
+        self.pieceMoved = board[self.fromRow][self.fromCol]
+        self.pieceCaptured = board[self.toRow][self.toCol]
+
+    
+    def convertToChessNotation(self) -> str:
+        return self.convertToRankFile(self.fromRow, self.fromCol) + self.convertToRankFile(self.toRow, self.toCol)
+    
+    def convertToRowCol(self, rank, file) -> list[int]:
+        return
+        
+    def convertToRankFile(self, row, col) -> str:
+        return self.CONV_COLS_TO_FILES[col] + self.CONV_ROWS_TO_RANK[row]
+        
+        
+    def __str__(self) -> str:
+        return self.convertToChessNotation()
+        
+    
 """
 Stores the information about the current state of a chess game.
 Determines the valid moves for the current gamestate.
 
 TODO: Use numpy for faster processing, especially when implementing AI engine
 """
+
 class GameState():
     def __init__(self):
         self.board = [
@@ -20,39 +57,12 @@ class GameState():
         self.whiteToMove = True
         self.moveLog = []
         
-"""
-Stores the information of a move
-"""
-class Move():
-    CONV_RANK_TO_ROWS = {"1":7, "2":6, "3":5, "4":4, "5":3, "6":2, "7":1, "8":0}
-    CONV_FILES_TO_COLS = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7}
-    CONV_ROWS_TO_RANK = {7: "1", 6:"2", 5:"3", 4:"4", 3:"5", 2:"6", 1:"7", 0:"8"}
-    CONV_COLS_TO_FILES = {7: "h", 6:"g", 5:"f", 4:"e", 3:"d", 2:"c", 1:"b", 0:"a"}
-    
-    
-    def __init__(self, fromSquare, toSquare, gameState):
-        self.pieceMoved = gameState[self.fromRow][self.fromCol]
-        self.pieceCaptured = gameState[self.toRow][self.toCol]
+            
+    def move(self, move: Move) -> None:
+        self.board[move.fromRow][move.fromCol] = "--"
+        self.board[move.toRow][move.toCol] = move.pieceMoved
         
-        self.fromRow = fromSquare[0]
-        self.fromCol = fromSquare[1]
-        self.toRow = toSquare[0]
-        self.toCol = toSquare[1]
-    
-    def convertToChessNotation(self) -> str:
-        fromRank = Move.CONV_ROWS_TO_RANK[self.fromRow]
-        toRank = Move.CONV_ROWS_TO_RANK[self.toRow]
-        fromFile = Move.CONV_COLS_TO_FILES[self.fromCol]
-        toFile = Move.CONV_COLS_TO_FILES[self.toCol]
-        return str(fromRank)+str(fromFile)+"->"+str(toRank)+str(toFile)
-    
-    def convertToRowCol(self) -> [int, int]:
-        return
+        self.moveLog.append(move)
+        self.whiteToMove = False if self.whiteToMove else True
         
-        
-    def __str__(self):
-        return self.convertToChessNotation()
-        
-    
-    
         
